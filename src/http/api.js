db89e60763572565e771da0b70cfa3727b199d7b
soaringwind <3163053781@qq.com>
2023-06-14 23:05:23 +0800

    Update api.js

--- src/http/api.js
+++ src/http/api.js
@@ -28,7 +28,7 @@ export function getHttp(url, params = {}) {
  * @param data
  * @returns {Promise}
  */
-export function postHttp(url=address+'api/saveData', data = {}) {
+export function postHttp(url="http://119.3.238.207:5000/api/saveData", data = {}) {
     data = Qs.stringify({"data": data})
     return new Promise((resolve, reject) => {
         $http.post(url, data)
